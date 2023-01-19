cfd90f60421b890d69949d5d0973f23db041ab8f
bapakfair <fairsulaiman@gmail.com>
2023-01-19 19:52:50 +0700

    implementing go routine on get user

--- src/query/users/get.users.go
+++ src/query/users/get.users.go
@@ -20,6 +20,8 @@ import (
 // get users by query string "_id", "pokja", "fullName", "nomorAnggota".
 func GetUsers(c echo.Context) (models.ResponseWithPagination, error) {
 	var res models.ResponseWithPagination
+	dataLoop := make(chan []bson.M)
+	totalData := make(chan int)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	db, err := config.Connect()
 	if err != nil {
@@ -35,66 +37,75 @@ func GetUsers(c echo.Context) (models.ResponseWithPagination, error) {
 	findOptions.SetSkip((int64(page) - 1) * int64(perPage))
 	findOptions.SetLimit(int64(perPage))
 	//findOptions.SetProjection(bson.M{"_id": 1, "pokja": 1})
+	var dataFinal []bson.M
 
-	if q := c.QueryParam("q"); q != "" {
-		objId, _ := primitive.ObjectIDFromHex(q)
+	go func() {
+		defer close(dataLoop)
+		if q := c.QueryParam("q"); q != "" {
+			objId, _ := primitive.ObjectIDFromHex(q)
 
-		filter = bson.M{
-			"role": 99,
-			"$or": []bson.M{
-				{
-					"_id": objId,
-				}, {
-					"nomorAnggota": bson.M{
-						"$regex": primitive.Regex{
-							Pattern: q,
-							Options: "i",
+			filter = bson.M{
+				"role": 99,
+				"$or": []bson.M{
+					{
+						"_id": objId,
+					}, {
+						"nomorAnggota": bson.M{
+							"$regex": primitive.Regex{
+								Pattern: q,
+								Options: "i",
+							},
 						},
-					},
-				}, {
-					"fullName": bson.M{
-						"$regex": primitive.Regex{
-							Pattern: q,
-							Options: "i",
+					}, {
+						"fullName": bson.M{
+							"$regex": primitive.Regex{
+								Pattern: q,
+								Options: "i",
+							},
 						},
-					},
-				}, {
-					"pokja": bson.M{
-						"$regex": primitive.Regex{
-							Pattern: q,
-							Options: "i",
+					}, {
+						"pokja": bson.M{
+							"$regex": primitive.Regex{
+								Pattern: q,
+								Options: "i",
+							},
 						},
 					},
 				},
-			},
+			}
+		} else if q == "" {
+			filter = bson.M{
+				"role": 99,
+			}
 		}
-	} else if q == "" {
-		filter = bson.M{
-			"role": 99,
+		data, err := db.Collection("users").Find(ctx, filter, findOptions)
+		if err != nil {
+			return
 		}
-	}
-	data, err := db.Collection("users").Find(ctx, filter, findOptions)
-	if err != nil {
-		return res, err
-	}
-	var dataFinal []bson.M
-	if err := data.All(ctx, &dataFinal); err != nil {
-		return res, err
-	}
+		if err := data.All(ctx, &dataFinal); err != nil {
+			return
+		}
+		dataLoop <- dataFinal
+	}()
 
-	if len(dataFinal) == 0 {
+	dataFromChan := <-dataLoop
+	if len(dataFromChan) == 0 {
 		err = errors.New("no documents in result")
 		return res, err
 	}
 
-	totalData, err := db.Collection("users").CountDocuments(ctx, filter)
-	if err != nil {
-		return res, err
-	}
-	totalDataCount := int(totalData) / perPage
-	if totalDataCount%1 != 0 {
-		totalDataCount = totalDataCount + 1
-	}
+	go func() {
+		defer close(totalData)
+		totalDataDB, err := db.Collection("users").CountDocuments(ctx, filter)
+		if err != nil {
+			return
+		}
+		totalDataCount := int(totalDataDB) / perPage
+		if totalDataCount%1 != 0 {
+			totalDataCount = totalDataCount + 1
+		}
+		totalData <- totalDataCount
+	}()
 
 	// this line of code below to manual hash nik & kk data from string to hashed vice versa =======================
 	// don't forget to change context timeout 120 second per 1000 data
@@ -108,91 +119,122 @@ func GetUsers(c echo.Context) (models.ResponseWithPagination, error) {
 	//fmt.Println(hasil)
 	// ==================================================================================================
 
-	for i := 0; i < len(dataFinal); i++ {
-
+	for _, data := range dataFromChan {
 		secret := os.Getenv("RAHASIA_NEGARA")
-		NIK := fmt.Sprintf("%v", dataFinal[i]["nik"])
-		KK := fmt.Sprintf("%v", dataFinal[i]["kk"])
-		//Phone := fmt.Sprintf("%v", dataFinal[i]["phoneNumber"])
-		//Alamat := fmt.Sprintf("%v", dataFinal[i]["alamat"])
-		dataFinal[i]["nik"] = string(helper.Decrypt([]byte(NIK), secret))
-		dataFinal[i]["kk"] = string(helper.Decrypt([]byte(KK), secret))
+		NIK := fmt.Sprintf("%v", data["nik"])
+		KK := fmt.Sprintf("%v", data["kk"])
+		dataKK := make(chan string)
+		dataNIK := make(chan string)
+		jumlahPetak := make(chan int64)
+		lahanGarapan := make(chan []bson.M)
 
-		checkPetak, err := db.Collection("petak").Find(ctx, bson.M{"userId": dataFinal[i]["_id"]})
+		Phone := fmt.Sprintf("%v", data["phoneNumber"])
+		if Phone == "<nil>" {
+			data["phoneNumber"] = ""
+		} else {
+			data["phoneNumber"] = string(helper.Decrypt([]byte(Phone), secret))
+		}
+
+		Alamat := fmt.Sprintf("%v", data["alamat"])
+		fmt.Println(Alamat)
+		if len(Alamat) < 8 {
+			data["alamat"] = Alamat
+		} else {
+			data["alamat"] = string(helper.Decrypt([]byte(Alamat), secret))
+		}
+		go func() {
+			defer close(dataNIK)
+			dataNIK <- string(helper.Decrypt([]byte(NIK), secret))
+		}()
+		go func() {
+			defer close(dataKK)
+			dataKK <- string(helper.Decrypt([]byte(KK), secret))
+		}()
+
+		data["nik"] = <-dataNIK
+		data["kk"] = <-dataKK
+
+		checkPetak, err := db.Collection("petak").Find(ctx, bson.M{"userId": data["_id"]})
 		if err != nil {
 			return res, err
 		}
 
-		var data []bson.M
-		if err := checkPetak.All(ctx, &data); err != nil {
+		var dataPetak []bson.M
+		if err := checkPetak.All(ctx, &dataPetak); err != nil {
 			err := errors.New("binding cursor gagal")
 			return res, err
 		}
 
-		if len(data) == 0 {
-			dataFinal[i]["jumlahPetak"] = 0
-			dataFinal[i]["totalLahanGarapan"] = 0
+		if len(dataPetak) == 0 {
+			data["jumlahPetak"] = 0
+			data["totalLahanGarapan"] = 0
 		} else {
 			// menghitung jumlah petak yang dimiliki anggota =============
-			jumlahPetakUser, err := db.Collection("petak").CountDocuments(ctx, bson.M{
-				"pokja":  dataFinal[i]["pokja"],
-				"userId": dataFinal[i]["_id"],
-			})
-			if err != nil {
-				return res, err
-			}
+			go func() {
+				defer close(jumlahPetak)
+				jumlahPetakUser, err := db.Collection("petak").CountDocuments(ctx, bson.M{
+					"pokja":  data["pokja"],
+					"userId": data["_id"],
+				})
+				if err != nil {
+					return
+				}
+				jumlahPetak <- jumlahPetakUser
+			}()
+
 			// ============================================================
 
 			// menghitung total luas lahan garapan ========================
-			matchStage := bson.D{
-				{"$match", bson.D{
-					{"userId", dataFinal[i]["_id"]},
-					{"pokja", bson.D{{"$exists", true}}},
-				}},
-			}
-			groupStage := bson.D{
-				{"$group", bson.D{
-					{"_id", "$pokja"},
-					{"luasLahan", bson.D{
-						{"$sum", bson.D{
-							{"$toDecimal", "$luasLahan"},
+			go func() {
+				defer close(lahanGarapan)
+				matchStage := bson.D{
+					{"$match", bson.D{
+						{"userId", data["_id"]},
+						{"pokja", bson.D{{"$exists", true}}},
+					}},
+				}
+				groupStage := bson.D{
+					{"$group", bson.D{
+						{"_id", "$pokja"},
+						{"luasLahan", bson.D{
+							{"$sum", bson.D{
+								{"$toDecimal", "$luasLahan"},
+							}},
 						}},
 					}},
-				}},
-			}
+				}
 
-			projectStage := bson.D{
-				{"$project", bson.D{
-					{"luasLahan", bson.D{
-						{"$round", bson.A{
-							"$luasLahan", 2,
+				projectStage := bson.D{
+					{"$project", bson.D{
+						{"luasLahan", bson.D{
+							{"$round", bson.A{
+								"$luasLahan", 2,
+							}},
 						}},
 					}},
-				}},
-			}
-			cursor, err := db.Collection("petak").Aggregate(ctx, mongo.Pipeline{matchStage, groupStage, projectStage})
-			if err != nil {
-				return res, err
-			}
-			var results []bson.M
-			if err = cursor.All(context.TODO(), &results); err != nil {
-				return res, err
-			}
+				}
+				cursor, err := db.Collection("petak").Aggregate(ctx, mongo.Pipeline{matchStage, groupStage, projectStage})
+				if err != nil {
+					return
+				}
+				var results []bson.M
+				if err = cursor.All(context.TODO(), &results); err != nil {
+					return
+				}
+				lahanGarapan <- results
+			}()
 
 			// =============================================================
-
-			dataFinal[i]["jumlahPetak"] = jumlahPetakUser
-			dataFinal[i]["totalLahanGarapan"] = results[0]["luasLahan"]
+			lahanGarapanDariChan := <-lahanGarapan
+			data["jumlahPetak"] = <-jumlahPetak
+			data["totalLahanGarapan"] = lahanGarapanDariChan[0]["luasLahan"]
 		}
-
-		//fmt.Println(i, " ini tersangka usernya: ", dataFinal[i], "-> ", results[0]["luasLahan"])
-		//dataFinal[i]["phoneNumber"] = string(helper.Decrypt([]byte(Phone), secret))
-		//dataFinal[i]["alamat"] = string(helper.Decrypt([]byte(Alamat), secret))
 	}
+
 	res.Message = "Get data success"
 	res.Data = dataFinal
 	res.Page = page
-	res.TotalData = int(totalData)
+	res.TotalData = <-totalData
 
 	return res, nil
 }
